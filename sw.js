54c74f7600ff8e24767b12ab79bc00d85aed9e17
Dingus-Droid <jellismillermakesitwork@gmail.com>
2026-05-21 16:55:47 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'rsvp4dingus-v1';
+const ASSETS = [
+  '/brotherboard/',
+  '/brotherboard/index.html',
+  '/brotherboard/manifest.json',
+  '/brotherboard/icon-192.png',
+  '/brotherboard/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
